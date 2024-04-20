040b1927b56b33947c5fdf77da84a094de5b9ff8
renjundong <r583500184@163.com>
2024-04-20 22:44:35 +0800

    feat:初始化项目;

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  alias:{
-    "@": "/<srcDir>",
-  },
-  vite: {
-    vue: {
-      customElement: true
-    },
-    vueJsx: {
-      mergeProps: true
-    }
-  }
+
 })
