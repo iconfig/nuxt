a9bffd35ef065acd68d42492f4fd6513fec54fc5
renjundong <r583500184@163.com>
2024-04-22 15:21:08 +0800

    feat:首页;

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    关于
+     <ul>
+         <li v-for="(item,index) in list" :key="index">{{item.name}}</li>
+     </ul>
+</div>
+</template>
+
+<script lang="ts" setup>
+let {data:list} = await useFetch('/api/channel')
+</script>
+
+<style>
+
+</style>
