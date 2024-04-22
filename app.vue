e383e5e797657b3e90c0ef4300994d6c2dc981cc
renjundong <r583500184@163.com>
2024-04-22 15:17:24 +0800

    feat:数据接口;

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+useSeoMeta({
+    title:'留言板',
+    //网站的描述
+    description:'',
+    //搜索关键词
+    keywords:''
+})
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtPage/>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  //nuxt的一个调试工具
   devtools: { enabled: true },
-
+  //开启ssr
+  ssr:true,
+  pages:true
 })
